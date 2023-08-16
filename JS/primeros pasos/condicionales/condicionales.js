5d6b26fd9adb78400256f6f28d55ef2a59ace495
Giogap <giogap@gmail.com>
2023-08-16 17:34:35 -0500

    upload

---
+++ JS/primeros pasos/condicionales/condicionales.js
@@ -0,0 +1,35 @@
+'use strict'
+
+// condicional if
+
+var edad = 30;
+var edad1 = 75;
+var edad2 = 12;
+var edad3 = 17;
+var edad4 = 10;
+var edad5 = 25;
+
+if (edad > edad2) {
+    console.log("Edad 1 es Mayor a Edad 2");
+} else {
+    console.log("Edad es Inferior");
+}
+
+if (edad3 >= 18) {
+    console.log("Es Mayor de Edad");
+} else {
+    console.log("Es Menor de Edad");
+}
+
+// condicional y
+
+if (edad > 10 && edad < 40) {
+    console.log(edad);
+}
+
+if (edad4 < edad2) {
+    console.log(edad4);
+} else {
+    console.log(edad2);
+}
+
---
+++ JS/primeros pasos/condicionales/switch.js
@@ -0,0 +1,17 @@
+'use strict'
+
+var edad = 30;
+var edad1 = 75;
+var edad2 = 12;
+var edad3 = 17;
+var edad4 = 10;
+var edad5 = 25;
+var imprime = "";
+
+switch(edad) {
+    case 30:
+        imprime = "Eres Mayor de Edad";
+    break;
+}
+    
+console.log(imprime);
